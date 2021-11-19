77aa0c447876ab61de86df3195b8a1303816b273
ecandotti <enzo.candotti16@hotmail.com>
2021-11-19 12:39:00 +0100

    test ?

---
+++ ArchiTest/DummyDataDBInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ArchiAPI.Data;
+using ArchiAPI.Models;
+
+namespace ArchiTest
+{
+    public class DummyDataDBInitializer  
+    {  
+        public DummyDataDBInitializer()  
+        {  
+        }  
+  
+        public void Seed(ArchiDbContext context)  
+        {  
+            context.Database.EnsureDeleted();  
+            context.Database.EnsureCreated();
+
+            context.Pizzas.AddRange(
+                new Pizza() { Name = "Royal", Price = 13, Topping = "Champignon, Formage" },
+                new Pizza() { Name = "Chevre Miel", Price = 11, Topping = "Chevre, Miel" }
+            );
+
+            context.SaveChanges();  
+        }  
+    }  
+}
